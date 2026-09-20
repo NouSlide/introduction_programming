30f5e1634b6e1d7aa210eaefc5e6fe207844e2f2
NouSlide <134683470+NouSlide@users.noreply.github.com>
2026-09-20 11:21:11 +0300

    Update medium_level.cpp

--- lab2/medium_level.cpp
+++ lab2/medium_level.cpp
@@ -10,14 +10,15 @@ int main() {
 	int sec;
 	cout << "Введите кол-во секунд: ";
 
-	while (!(cin >> sec)) {
-		cout << "Вы ввели не число: ";
+	while (!(cin >> sec) || sec <= 0) {
+		cout << "Введите число больше 0: ";
 		cin.clear();
 		std::cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
 	}
 
 	auto hours = sec / 3600;
-	cout << "Результат в часах(целое): " << hours << endl << "и остаток от деления: " << sec % 3600 << endl;
+	auto rem = sec % 3600;
+	cout << "Результат в часах(целое): " << hours << endl << "и остаток от деления: " << rem << endl;
 
 	return 0;
 }
